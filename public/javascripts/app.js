8d8224293d4c6f434da68778f10a7aae89d9d53f
srid99 <srid99@gmail.com>
2014-07-20 23:53:57 +0200

    [MINOR] Added shortcut key(s) to set focus on search box

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,24 @@
+var App = (function() {
+    var searchBox;
 
+    var setFocusOnSearchBox = function() {
+        searchBox.focus();
+    };
+
+    return {
+        init: function() {
+            searchBox = $("input[name='q']");
+            return this;
+        },
+
+        bindEvents: function() {
+            $(document).bind('keydown', 'Ctrl+Shift+s', setFocusOnSearchBox);
+            $(document).bind('keydown', 'Ctrl+Shift+f', setFocusOnSearchBox);
+            return this;
+        }
+    };
+})();
+
+$(document).ready(function() {
+    App.init().bindEvents();
+});
